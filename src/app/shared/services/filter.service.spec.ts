295eec93cf7aca095bee2f0078e3061944fc7ed9
Zoran Vulanovic <vulezor@gmail.com>
2022-03-24 03:39:33 +0100

    added unit test

--- src/app/shared/services/filter.service.spec.ts
+++ src/app/shared/services/filter.service.spec.ts
@@ -15,4 +15,12 @@ describe('FilterService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should set and get filter', () => {
+    const filter = { key: 'test', value: 'test' };
+    service.setFilter(filter);
+    service.getFilter().subscribe((f) => {
+      expect(f).toEqual(filter);
+    });
+  });
 });
